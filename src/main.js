92fdb6b44bab1d095970ea455e272af11cc37e87
jiaqi <2468260248@qq.com>
2026-08-10 17:05:08 +0800

    fix: 补齐渲染一致性所需全局挂载
    - main.js 挂载 \/\（Vue.prototype，lowcode 23 个文件依赖）
    - 引入 vue_util.js 全局方法（recoverFileAddress4richText/getImagePath 等）
    - 移除 vue_util 中 dialog.vue 表单集群依赖（popupDialog 无使用）
    - 引入主题样式 theme/scss/common-theme.scss
    - 移除 @vue/babel-preset-jsx（JSX 已模板化）
    - vite 配置 extensions 支持无扩展名 .vue 导入、commonjsOptions 支持 UMD 播放器

--- src/main.js
+++ src/main.js
@@ -5,12 +5,20 @@ import store from "./store";
 
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import "../theme/scss/common-theme.scss";
 
 import Fragment from "vue-fragment";
 Vue.use(Fragment.Plugin);
 
 import clickoutside from "@/directives/clickoutside.js";
 Vue.directive("clickoutside", clickoutside);
+
+import VueUtil from "@/components/common/vue_util.js";
+Vue.use(VueUtil);
+
+import { $http } from "@/common/http";
+Vue.prototype.$http = $http;
+Vue.prototype.$axios = $http;
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
